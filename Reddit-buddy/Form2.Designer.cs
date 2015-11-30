57f8c88921ae58c489d8575e256226a5f83d447d
Tomas Novikovas <scriverry@gmail.com>
2015-11-30 22:20:45 +0200

    Three working listviews

--- Reddit-buddy/Form2.Designer.cs
+++ Reddit-buddy/Form2.Designer.cs
@@ -30,12 +30,16 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Location = new System.Drawing.Point(63, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
@@ -44,29 +48,65 @@
             // listView1
             // 
             this.listView1.Activation = System.Windows.Forms.ItemActivation.OneClick;
-            this.listView1.Location = new System.Drawing.Point(12, 39);
+            this.listView1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.listView1.Location = new System.Drawing.Point(183, 5);
             this.listView1.Name = "listView1";
-            this.listView1.Scrollable = false;
-            this.listView1.Size = new System.Drawing.Size(602, 226);
+            this.listView1.Size = new System.Drawing.Size(712, 286);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.List;
             this.listView1.Click += new System.EventHandler(this.listView1_Click);
             // 
+            // listView2
+            // 
+            this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView2.Location = new System.Drawing.Point(540, 297);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(527, 329);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // listView3
+            // 
+            this.listView3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.listView3.Location = new System.Drawing.Point(12, 297);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(527, 329);
+            this.listView3.TabIndex = 4;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.List;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(133, 225);
+            this.panel1.TabIndex = 5;
+            // 
             // Form2
             // 
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1046, 570);
+            this.ClientSize = new System.Drawing.Size(1079, 638);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.listView1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.MaximumSize = new System.Drawing.Size(1190, 701);
+            this.MinimumSize = new System.Drawing.Size(1095, 677);
             this.Name = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Reddit-buddy/Form2.cs
+++ Reddit-buddy/Form2.cs
@@ -16,22 +16,56 @@ namespace Reddit_buddy
     public partial class Form2 : Form
     {
         Subreddit default_subredit;
+        Subreddit bottom_left_subreddit;
+        Subreddit bottom_right_subreddit;
+
         List<Post> default_subreddit_posts = new List<Post>();
+        List<Post> bottom_left_posts = new List<Post>();
+        List<Post> bottom_right_posts = new List<Post>();
+
 
         public Form2(Reddit reddit)
         {
             InitializeComponent();
-            default_subredit = reddit.GetSubreddit("/r/programming");
-            Debug.WriteLine(default_subredit.GetType());
+            default_subredit = reddit.RSlashAll;
             label1.Text = "/r/" + default_subredit.Name;
             listView1.View = View.Details;
             listView1.Columns.Add("Title", - 2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Subreddit", -2, HorizontalAlignment.Left);
             listView1.Columns.Add("Score", -2, HorizontalAlignment.Left);
+            listView1.Columns[0].Width = (listView1.Width / 7) * 6;
+            listView1.Columns[1].AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
+            listView1.Columns[2].AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
             foreach (var post in default_subredit.Hot.Take(15))
             {
                 default_subreddit_posts.Add(post);
-                listView1.Items.Add(new ListViewItem(new[] { post.Title, post.Score.ToString() }));
+                listView1.Items.Add(new ListViewItem(new[] { post.Title, post.SubredditName, post.Score.ToString() }));
             }
+
+            bottom_left_subreddit = reddit.GetSubreddit("/r/programming");
+            listView2.View = View.Details;
+            listView2.Columns.Add("Title", -2, HorizontalAlignment.Left);
+            listView2.Columns.Add("Score", -2, HorizontalAlignment.Left);
+            listView2.Columns[0].Width = (listView2.Width / 9) * 8;
+            listView2.Columns[1].AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
+            foreach (var post in bottom_left_subreddit.Hot.Take(15))
+            {
+                bottom_left_posts.Add(post);
+                listView2.Items.Add(new ListViewItem(new[] { post.Title, post.Score.ToString() }));
+            }
+
+            bottom_right_subreddit = reddit.GetSubreddit("/r/linux");
+            listView3.View = View.Details;
+            listView3.Columns.Add("Title", -2, HorizontalAlignment.Left);
+            listView3.Columns.Add("Score", -2, HorizontalAlignment.Left);
+            listView3.Columns[0].Width = (listView3.Width / 9) * 8;
+            listView3.Columns[1].AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
+            foreach (var post in bottom_right_subreddit.Hot.Take(15))
+            {
+                bottom_right_posts.Add(post);
+                listView3.Items.Add(new ListViewItem(new[] { post.Title, post.Score.ToString() }));
+            }
+
         }
 
         private void listView1_Click(object sender, EventArgs e)
@@ -43,7 +77,45 @@ namespace Reddit_buddy
                 {
                     if (post.Title == firstSelectedItem)
                     {
-                        System.Diagnostics.Process.Start(post.Url.ToString());
+                        Process.Start(post.Url.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Link opening failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listView2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var firstSelectedItem = listView1.SelectedItems[0].Text;
+                foreach (var post in bottom_left_posts)
+                {
+                    if (post.Title == firstSelectedItem)
+                    {
+                        Process.Start(post.Url.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Link opening failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listView3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var firstSelectedItem = listView1.SelectedItems[0].Text;
+                foreach (var post in bottom_right_posts)
+                {
+                    if (post.Title == firstSelectedItem)
+                    {
+                        Process.Start(post.Url.ToString());
                     }
                 }
             }
